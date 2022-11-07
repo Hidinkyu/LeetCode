69fcc114dec66d98f1144f465adf75f68080a9b6
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2022-11-06 18:56:45 -0800

    Time: 79 ms (92.24%), Space: 44.1 MB (86.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,7 +6,6 @@ var isPalindrome = function(s) {
     s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
     let i = 0;
     let j = s.length - 1;
-    console.log(s)
     
     while (i < j) {
         if (s[i] !== s[j]) return false;
