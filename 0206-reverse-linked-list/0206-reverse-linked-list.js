f026c66fde8951d4d0f440a03337ebe5f4b911ec
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2022-11-07 15:48:28 -0800

    Time: 114 ms (32.24%), Space: 45.3 MB (5.94%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,14 +9,30 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) { 
-  let [prev, cur] = [null, head];
+
+//  iterative Time complexit O(n) Space complexity O(1)
+
+// const reverseList = (head) => { 
+//   let [prev, cur] = [null, head];
   
-  while (cur) {
-    let next = cur.next; 
-    cur.next = prev;
-    prev = cur;
-    cur = next;
+//   while (cur) {
+//     let next = cur.next; 
+//     cur.next = prev;
+//     prev = cur;
+//     cur = next;
+//   }
+//   return prev
+// };
+
+// recursive Time complexity O(n) Space complexity O(n)
+
+const reverseList = (head) => {
+  if (!head) return null;
+  let newHead = head
+  if (head.next) {
+    newHead = reverseList(head.next);
+    head.next.next = head
   }
-  return prev
-};
+  head.next = null;
+  return newHead;
+}
