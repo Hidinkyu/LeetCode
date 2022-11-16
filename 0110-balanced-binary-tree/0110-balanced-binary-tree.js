8cd0919f0aaab105d83161361645611177b2fd6e
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2022-11-15 22:44:53 -0800

    Time: 145 ms (15.50%), Space: 47.2 MB (54.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -10,16 +10,28 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
-    return count_child(root) !== -1
-    
-    function count_child(root) {
-        if (!root) return 0
-        const left = count_child(root.left)
-        const right = count_child(root.right)
-        
-        if (left === -1 || right === -1 || Math.abs(left - right) > 1) return -1
-        
-        return 1 + Math.max(left, right)
-    }
+
+/*
+prompt
+  Given a binary tree, determine if it is height-balanced. 
+  For this problem, a height- balanced binary tree is defined as:
+    a binary tree in which the left and right subtrees of EVERY node differ in height by no more than 1
+
+  The question is asking to check and see if the tree has a difference of its left and right subtrees heights are at most 1.
+  As an example we can check the root node of the example diagramed to the left (Example 1, 110. Balanced Binary Tree leetcode); 
+The root node has a leftMaxHeight of 1 and a rightMaxHeight of 2.
+  Taking the absolute value of the leftMaxHeight and rightMaxHeight's difference shows that the left and right sub trees has a maximum difference one only 1. This passes the conditions set by the prompt of only having a maximum difference in height by 1.
+  The most important thing to take note of is that the prompt asks to do this for not only the root node, but for all its subsequent nodes.
+  Starting off we can instinctually just run a dfs on the left sub-tree and the right-subtree extracting both heights and then returning its values difference. This approach will work, but it is quite inefficient in time because for every node that we visit, the recursive call that runs in O(n) time complexity will be invoked which will be n times, so in the end the time complexity will be O(n) * n evaluating to O(n^2);
+  So instead of doing this problem from top to bottom (pre-order dfs), let's try from bottom to top (in-order DFS)
+*/
+
+var isBalanced = function(root) {   
+  return dfs(root) !== -1;
+  function dfs(root) {
+    if (!root) return 0
+    const [left, right] = [dfs(root.left), dfs(root.right)];
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) return -1;
+    return 1 + Math.max(left, right)
+  } 
 };
