2779c84e4a6ac2296300d5e95c760c2b44691dc0
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2023-08-01 13:32:34 -0700

    Time: 99 ms (99.20%), Space: 47.1 MB (71.61%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+// Create a mapping of Roman numeral symbols to their integer values
   const romanToInt = {
     'I': 1,
     'V': 5,
@@ -13,21 +14,28 @@ var romanToInt = function(s) {
     'M': 1000
   };
 
+  // Initialize variables to store the total result and the previous value
   let total = 0;
   let prevValue = 0;
 
+  // Iterate through the Roman numeral string from right to left
   for (let i = s.length - 1; i >= 0; i--) {
     const symbol = s[i];
     const value = romanToInt[symbol];
 
+    // If the current value is smaller than the previous value, it means subtraction is used
+    // Subtract the current value from the total
+    // Otherwise, add the current value to the total
     if (value < prevValue) {
       total -= value;
     } else {
       total += value;
     }
 
+    // Update the previous value for the next iteration
     prevValue = value;
   }
 
+  // Return the final total as the result
   return total;
 };
