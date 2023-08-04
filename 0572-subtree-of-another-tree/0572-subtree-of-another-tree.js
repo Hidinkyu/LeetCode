c8707dc130947bebff11437e7b7c442fc4a952ed
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2023-08-04 14:25:56 -0700

    Time: 80 ms (77.43%), Space: 49.1 MB (68.90%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -1,22 +1,16 @@
-function isSubtree(root, subRoot) {
-    if (root === null) {
-        return false; // Base case: if we have traversed past leaf, there's no match.
-    } else if (isSameTree(root, subRoot)) {
-        return true; // Return true if we find a match.
-    } else {
-        // Otherwise, keep traversing.
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-    }
-}
+const isSubtree = (s, t) => {
+    if (!t) return true;
+    else if (!s) return false;
+    else if (compare(s, t)) return true;
+    else return (isSubtree(s.left, t) || isSubtree(s.right, t));
+};
 
-function isSameTree(root1, root2) {
-    if (root1 === null || root2 === null) {
-        return root1 === root2; // If either are null, return true only if both are.
-    } else if (root1.val === root2.val) {
-        // If the values of the roots are the same, check the children.
-        return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
-    } else {
-        // If the values are not the same, return false.
-        return false;
+const compare = (s, t) => {
+    if (!s && !t) return true;
+
+    if (s && t && s.val === t.val) {
+        return compare(s.left, t.left) && compare(s.right, t.right);
     }
+
+    return false;
 }
