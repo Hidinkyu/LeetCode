2604623b0d4d3cb55b75e6bc41cf4408c83f3c37
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2023-08-07 10:45:40 -0700

    Time: 76 ms (56.94%), Space: 46.3 MB (18.23%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,9 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    const arr = nums.slice();
-    for (let i = 0; i < nums.length; i++) {
-        arr.push(nums[i]);
-    }
-    return arr;
+    return [...nums, ...nums]
 };
