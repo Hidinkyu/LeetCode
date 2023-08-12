f672c43fe4fcbb1cf7bb316cab6842754a43234e
Hidinkyu <99449345+Hidinkyu@users.noreply.github.com>
2023-08-11 18:24:03 -0700

    Time: 72 ms (99.44%), Space: 54.2 MB (60.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -5,15 +5,18 @@
 var productExceptSelf = function(nums) {
     const res = [nums[0]];
     for (let i = 1; i < nums.length; i++) {
-        let product = nums[i];
-        product *= res[i-1]
-        res.push(product)
+        let product = nums[i] * res[i - 1];
+        res.push(product);
     }
-    let product = 1;
-    for (let i = nums.length - 1; i > 0; i--) {
-        res[i] = res[i - 1] * product;
-        product *= nums[i]
+    
+    let product = 1; 
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (res[i - 1] || res[i - 1] === 0) {
+            res[i] = res[i - 1] * product;
+            product *= nums[i];
+        }
+        else res[i] = product;
     }
-    res[0] = product;
-    return res;
+    return res
 };
